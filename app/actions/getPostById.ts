5d1ac94a6cb8bc51a912f0eca350688da6a7d92e
Shoya Fukunaga <s.24jsb.gene@gmail.com>
2024-09-10 17:00:07 +0900

    IDから記事を取得

---
+++ app/actions/getPostById.ts
@@ -0,0 +1,26 @@
+import prisma from '../lib/prisma';
+
+interface Post {
+    id: number;
+}
+
+export async function getPostById(id: number) {
+    try {
+        const post = await prisma.post.findUnique({
+            where: {
+                id: id,
+            },
+        });
+
+        if (post) {
+            return post;
+        }
+        return null;
+    } catch (error) {
+        console.error('IDからの記事取得に失敗しました', error);
+        throw new Error('IDから記事を取得できません');
+
+
+
+    }
+}
