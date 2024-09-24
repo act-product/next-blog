9a2a582dd2403311d8b7e6f53d12f49c6aeb10e7
Shoya Fukunaga <s.24jsb.gene@gmail.com>
2024-09-24 09:28:14 +0900

    コードの簡易化

--- app/actions/getPostById.ts
+++ app/actions/getPostById.ts
@@ -6,16 +6,11 @@ interface Post {
 
 export async function getPostById(id: number) {
     try {
-        const post = await prisma.post.findUnique({
+        return await prisma.post.findUnique({
             where: {
                 id: id,
             },
         });
-        console.log('Post fetched by ID:', post);
-        if (post) {
-            return post;
-        }
-        return null;
     } catch (error) {
         console.error('IDからの記事取得に失敗しました', error);
         throw new Error('IDから記事を取得できません');
