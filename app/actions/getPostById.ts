bc43edf52254457eb62e51c9b19dc8ba89b5bf42
Shoya Fukunaga <s.24jsb.gene@gmail.com>
2024-09-25 09:13:47 +0900

    不要な行の削除

--- app/actions/getPostById.ts
+++ app/actions/getPostById.ts
@@ -1,23 +1,3 @@
-// import prisma from '../lib/prisma';
-
-// interface Post {
-//     id: number;
-// }
-
-// export async function getPostById(id: number) {
-//     try {
-//         return await prisma.post.findUnique({
-//             where: {
-//                 id: id,
-//             },
-//         });
-//     } catch (error) {
-//         console.error('IDからの記事取得に失敗しました', error);
-//         throw new Error('IDから記事を取得できません');
-//     }
-// }
-
-
 import prisma from '../lib/prisma';
 
 export async function getPostById(id: number) {
